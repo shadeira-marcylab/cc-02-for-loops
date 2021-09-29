d1981e6b0d499a1ee9f4cf12ddb95e6382c869a2
shadeira <shadeira@marcylabschool.org>
2021-09-29 15:57:49 +0000

    added student work to cc2

--- index.js
+++ index.js
@@ -1,25 +1,113 @@
-//1
+
 function countToTen(){
+   for(let i = 1; i < 10; i++){
+        console.log(i)
   
+   }
 }
 
+countToTen()
+
 //2
-function countFromOne(){
+function countFromOne(n){
+    for(let i = 1; i <= n; i++){
+        console.log(i)
   
+    }
 }
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(n){
+  for (let i=0; i<=n; i+=2) {
+        if (i > 1) {
+                console.log(i);
+        }
+    
+    }
 }
+countEveryEven(10)
+    
+
+
+//4
+function countEveryOdd(n){
+ for (let i=1; i<=n; i+=2) {
+                console.log(i);
+    
+    }
+}
+countEveryOdd(10) 
+
+
+//5 
+function countEvens(array){
+    let count = 0
+    for (let i=0; i<=array.length; i++) {
+        if (array[i]%2 === 0) {
+               count++
+        }
+    }
+       return count 
+    }
+
+
+
+
+
+
+
+//1 - Student answers
+
+
+function countToTen(){
+  for (let i =1; i < 11; i++){
+      console.log(i)
+  }
+}
+ countToTen()
+
+function countToTen(){
+  for (let i =1; i <=10; i++){
+      console.log(i)
+  }
+}
+ countToTen()
+
+//2
+function countFromOne(int){
+   for(let i = 1; i < int +1; i++){
+       console.log(i)
+   }
+}
+countFromOne(25)
+
+//3
+function countEveryEven(n){
+  for(let i = 1; i <= n; i++){
+		if(i % 2 !== 0){
+		console.log(i)
+		}
+	}
+}
+countEveryEven(10)
+
+
+    
+
 
 //4
 function countEveryOdd(){
-  
+ 
 }
 
-//5 
+
+
+
+
+
+//****5 
 function countEvens(){
-  
-}
+    
+    }
+
 
